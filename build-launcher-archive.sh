cf8ba840227bad26d6a4ac2ce23a6e20f8044bfb
ALEZ-DEV <raidersfocus@gmail.com>
2024-10-01 19:30:24 +0200

    update manifest file

--- build-launcher-archive.sh
+++ build-launcher-archive.sh
@@ -1,22 +1,22 @@
+#!/bin/sh
+
 # Build the Flutter app and package into an archive.
 # Exit if any command fails
 set -e
- 
+
 # Echo all commands for debug purposes
 set -x
- 
-projectName=Babylonia-terminal
- 
-archiveName=$projectName.tar.gz
-baseDir=$(pwd)
- 
 
-# ----------------------------- Build Flutter app ---------------------------- #
- 
+archiveName=babylonia-terminal.tar.gz
+baseDir=$(pwd)
+
+cd ./Babylonia-terminal/babylonia_terminal_launcher
+
 rinf message
 flutter pub get
+flutter create --platform=linux .
 flutter build linux
- 
+
 cd build/linux/x64/release/bundle || exit
 tar -czaf $archiveName ./*
 mv $archiveName "$baseDir"/
