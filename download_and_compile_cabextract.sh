60abfc8825cfa5539e41bed3d2c819d352b4f7f2
ALEZ <38623465+ALEZ-DEV@users.noreply.github.com>
2024-10-07 14:37:44 +0200

    Update download_and_compile_cabextract.sh

--- download_and_compile_cabextract.sh
+++ download_and_compile_cabextract.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 wget https://www.cabextract.org.uk/cabextract-1.11.tar.gz
-tar -zxf cabextract-1.11.tar.gz -C cabextract
+mkdir cabextract
+tar -zxf cabextract-1.11.tar.gz -C cabextract --strip-components=1
 cd cabextract
 ./configure
 make
